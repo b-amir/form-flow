7c32c9578768ad86ba45a998d6846b0e749b7ab4
theamirm <theamirm@gmail.com>
2025-09-05 06:37:33 +0330

    refactor(types): restructure form element types for better type safety

--- src/types/form.ts
+++ src/types/form.ts
@@ -1,9 +1,21 @@
-export interface Element {
+export type ElementType = 'text' | 'checkbox';
+
+export interface BaseElement {
   id: string;
-  type: 'text' | 'checkbox';
+  type: ElementType;
   label: string;
   isRequired?: boolean;
 }
+
+export interface TextElement extends BaseElement {
+  type: 'text';
+}
+
+export interface CheckboxElement extends BaseElement {
+  type: 'checkbox';
+}
+
+export type Element = TextElement | CheckboxElement;
 
 export interface Form {
   id: string;
