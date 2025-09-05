91a39e1f4da86595a5fc6a03a56cea768eba75f7
theamirm <theamirm@gmail.com>
2025-09-05 06:37:51 +0330

    feat: export form components from index file

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,6 @@
-export {};
+export { FormRenderer } from './FormRenderer';
+export { TextInputField } from './TextInputField';
+export { CheckboxField } from './CheckboxField';
+export { FieldFactory } from './FieldFactory';
+export { ValidatedFormRenderer } from './ValidatedFormRenderer';
+export { LoadingFormRenderer } from './LoadingFormRenderer';
